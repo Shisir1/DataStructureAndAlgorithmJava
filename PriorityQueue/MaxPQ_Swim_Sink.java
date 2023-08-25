7481f5d671bda203bd7981dec5f20802229f32c9
Shisir Humagain <Shisirhumagain0@gmail.com>
2023-08-25 17:45:55 -0400

    Top-Down Reheapify (sink) in Max Heap added

--- PriorityQueue/MaxPQ_Swim_Sink.java
+++ PriorityQueue/MaxPQ_Swim_Sink.java
@@ -1,11 +1,11 @@
 package org.example.PriorityQueue;
 
-public class MaxPriorityQueue {
+public class MaxPQ_Swim_Sink {
 
     private Integer[] heap;
     private int n;  //stores the size of max heap
 
-    public MaxPriorityQueue(int capacity){
+    public MaxPQ_Swim_Sink(int capacity){
         heap = new Integer[capacity + 1];
         n = 0;
     }
@@ -23,6 +23,8 @@ public class MaxPriorityQueue {
         heap[n] = x;
         swim(n);
     }
+
+    //Bottom-up reheapify (swim)
     private void swim(int index ){
         while(index > 1 && heap[index/2] < heap[index]){
             int temp = heap[index];
@@ -45,8 +47,45 @@ public class MaxPriorityQueue {
         }
     }
 
+    public int deleteMax(){
+        int max = heap[1];
+        swap(1,n);
+        n--;
+
+        //now shift down from the top until its binary tree
+        sink(1);
+        heap[n + 1] = null;
+
+        if(n > 0 && (n == (heap.length - 1) / 4)){
+            resize(heap.length/2);
+        }
+
+        return max;
+    }
+
+    private void sink(int k) {
+        while (2*k <= n){
+            int j = 2*k;
+            if(j < n && heap[j] < heap[j + 1]){
+                j++;
+            }
+            if(heap[k] >= heap[j]){
+                break;
+            }
+            swap(k,j);
+            k = j;
+        }
+    }
+
+    public void swap(int a, int b){
+        int temp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = temp;
+    }
+
+
     public static void main(String[] args){
-        MaxPriorityQueue pq = new MaxPriorityQueue(3);
+        MaxPQ_Swim_Sink pq = new MaxPQ_Swim_Sink(3);
 //        System.out.println(pq.size());
 //        System.out.println(pq.isEmpty());
         pq.insert(4);
@@ -55,7 +94,12 @@ public class MaxPriorityQueue {
         pq.insert(6);
         pq.insert(1);
         pq.insert(3);
+
         System.out.println("Adjusted size : " + pq.size());
         pq.printMaxHeap();
+        pq.deleteMax();
+        System.out.println("\nAfter deletion:");
+        pq.printMaxHeap();
+
     }
 }
