07381fa69f294d7976af17621946dcdb51489e5c
shisir <shisirhumagain0@gmail.com>
2024-10-25 11:19:29 -0400

    file modified

--- hashing/HashTable.java
+++ hashing/HashTable.java
@@ -32,12 +32,41 @@ public class HashTable {
         return size == 0;
     }
     public void put(Integer key, String value){
+        if(key == null || value == null){
+            throw new IllegalArgumentException("Key or Value is null!!");
+        }
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        while (head != null){
+            if(head.key.equals(key)){
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        size++;
+        head = buckets[bucketIndex];
+        HashNode node = new HashNode(key, value);   //(key,value) --> null
+        node.next = head;
+        buckets[bucketIndex] = node;
+    }
 
+    private int getBucketIndex(Integer key){
+        return key % numOfBuckets;  //also can be buckets.length
     }
     public String get(Integer key){
         return null;
     }
     public String remove(Integer key){
         return null;
+    }
+
+    public static void main(String[] args) {
+        HashTable table = new HashTable(10);
+        table.put(105, "Tom");
+        table.put(21, "Sana");
+        table.put(21, "Harry");
+        table.put(31, "Shisir");
+        System.out.println(table.size());
     }
 }
